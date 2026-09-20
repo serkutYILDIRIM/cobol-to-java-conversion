34e19aab09e177e0020a5e97e606733b61991fa3
serkutYILDIRIM <serkutyldrm@gmail.com>
2026-09-21 00:04:51 +0300

    readme added

---
+++ conversions/rep001/20260920T203654Z-a7f3/java/src/main/java/com/example/salaryreport/SalaryReportApplication.java
@@ -0,0 +1,12 @@
+package com.example.salaryreport;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SalaryReportApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SalaryReportApplication.class, args);
+    }
+}
---
+++ conversions/rep001/20260920T203654Z-a7f3/java/src/main/java/com/example/salaryreport/exception/ReportIoException.java
@@ -0,0 +1,8 @@
+package com.example.salaryreport.exception;
+
+public class ReportIoException extends RuntimeException {
+
+    public ReportIoException(String message, Throwable cause) {
+        super(message, cause);
+    }
+}
