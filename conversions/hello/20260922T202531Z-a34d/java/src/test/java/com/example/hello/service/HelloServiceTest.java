1736777f14907643a1aaaa54a188143d90a7fdaf
serkutYILDIRIM <serkutyldrm@gmail.com>
2026-09-24 23:36:19 +0300

    some fixs

--- conversions/hello/20260922T202531Z-a34d/java/src/test/java/com/example/hello/service/HelloServiceTest.java
+++ conversions/hello/20260922T202531Z-a34d/java/src/test/java/com/example/hello/service/HelloServiceTest.java
@@ -7,14 +7,12 @@ import org.junit.jupiter.api.Test;
 class HelloServiceTest {
 
     private final HelloService helloService = new HelloService();
-
     @Test
     void returnsTheThirtyCharacterCobolDisplayField() {
         assertThat(helloService.getMessage())
                 .isEqualTo("HELLO FROM IBM COBOL          ")
                 .hasSize(30);
     }
-
     @Test
     void retainsNoStateBetweenInvocations() {
         assertThat(helloService.getMessage()).isEqualTo(helloService.getMessage());
